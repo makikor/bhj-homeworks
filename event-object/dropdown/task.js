e582e148f1505b14d533f4241fe0bb3b08c77efc
Maksim <Mikimiki89@yandex.ru>
2024-09-24 23:47:20 +0300

    1.4

--- event-object/dropdown/task.js
+++ event-object/dropdown/task.js
@@ -1,6 +1,17 @@
-let clickI = document.body.querySelector(".dropdown__value");
+let Dropdown = document.body.querySelector(".dropdown__value");
 let list = document.body.querySelector(".dropdown__list");
+let links = document.body.querySelectorAll(".dropdown__item");
 
-clickI.addEventListener('click', function(){
+Dropdown.addEventListener('click', function(){
     list.classList.toggle('dropdown__list_active');
-})
+    Array.from(links).forEach((i) => {
+        i.addEventListener("click", function(){
+            let value = i.textContent.trim();
+            Dropdown.textContent = value;
+            console.log(value)
+        })
+    })
+    }
+)
+
+
